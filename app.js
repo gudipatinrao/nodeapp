eba204684b9235b97b6a589c81f4d5479f86a308
gudipatinrao <narasimharao.gudipati@ibm.com>
2020-08-19 16:07:19 -0500

    2nd commit

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const app = require('express')()
 
 app.get('/', (req, res) => {
-  res.send("Hello from Appsody! quick demo");
+  res.send("Hello from IBM Appsody! quick demo");
 });
  
 module.exports.app = app;
